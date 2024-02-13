5b6f8c90e74e6da879d237070efb12aa7edd33a9
Averi Kitsch <akitsch@google.com>
2024-02-13 14:56:47 -0800

    lint

--- src/langchain_google_alloydb_pg/alloydb_engine.py
+++ src/langchain_google_alloydb_pg/alloydb_engine.py
@@ -17,15 +17,7 @@ from __future__ import annotations
 import asyncio
 from dataclasses import dataclass
 from threading import Thread
-from typing import (
-    TYPE_CHECKING,
-    Awaitable,
-    Dict,
-    List,
-    Optional,
-    TypeVar,
-    Union,
-)
+from typing import TYPE_CHECKING, Awaitable, Dict, List, Optional, TypeVar, Union
 
 import aiohttp
 import google.auth  # type: ignore
