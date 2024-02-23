d90402e063e86ffa727baf37f334c32707410b94
Averi Kitsch <akitsch@google.com>
2024-02-23 10:09:35 -0800

    feat: add create methods for Loader (#29)
    * feat: add create methods for Loader
    
    * fix metadata
    
    * Update docs/document_loader.ipynb
    
    Co-authored-by: Kurtis Van Gent <31518063+kurtisvg@users.noreply.github.com>
    
    * Update docs/document_loader.ipynb
    
    Co-authored-by: Kurtis Van Gent <31518063+kurtisvg@users.noreply.github.com>
    
    * Update document_loader.ipynb
    
    * clean
    
    ---------
    
    Co-authored-by: Kurtis Van Gent <31518063+kurtisvg@users.noreply.github.com>

--- src/langchain_google_alloydb_pg/alloydb_loader.py
+++ src/langchain_google_alloydb_pg/alloydb_loader.py
@@ -14,9 +14,7 @@
 
 from __future__ import annotations
 
-import asyncio
 import json
-from threading import Thread
 from typing import (
     Any,
     AsyncIterator,
@@ -37,7 +35,6 @@ from .alloydb_engine import AlloyDBEngine
 
 DEFAULT_CONTENT_COL = "page_content"
 DEFAULT_METADATA_COL = "langchain_metadata"
-DEFAULT_FORMAT = "text"
 
 
 def text_formatter(row, content_columns) -> str:
@@ -110,42 +107,146 @@ class AlloyDBLoader(BaseLoader):
     the `page_content` and everything else into the `metadata`.
     """
 
+    __create_key = object()
+
     def __init__(
         self,
+        key,
+        engine: AlloyDBEngine,
+        query: str,
+        content_columns: List[str],
+        metadata_columns: List[str],
+        formatter: Callable,
+        metadata_json_column: Optional[str] = None,
+    ) -> None:
+        if key != AlloyDBLoader.__create_key:
+            raise Exception(
+                "Only create class through 'create' or 'create_sync' methods!"
+            )
+
+        self.engine = engine
+        self.query = query
+        self.content_columns = content_columns
+        self.metadata_columns = metadata_columns
+        self.formatter = formatter
+        self.metadata_json_column = metadata_json_column
+
+    @classmethod
+    async def create(
+        cls,
         engine: AlloyDBEngine,
         query: Optional[str] = None,
         table_name: Optional[str] = None,
         content_columns: Optional[List[str]] = None,
         metadata_columns: Optional[List[str]] = None,
+        metadata_json_column: Optional[str] = None,
         format: Optional[str] = None,
         formatter: Optional[Callable] = None,
-        read_only: Optional[bool] = None,
-        time_out: Optional[int] = None,
-        metadata_json_column: Optional[str] = None,
-    ) -> None:
-        """Initialize Alloydb document loader."""
+    ):
+        """Constructor for AlloyDBLoader
 
-        self.engine = engine
-        self.table_name = table_name
-        self.query = query
+        Args:
+            engine (AlloyDBEngine):AsyncEngine with pool connection to the postgres database
+            query (Optional[str], optional): SQL query. Defaults to None.
+            table_name (Optional[str], optional): Name of table to query. Defaults to None.
+            content_columns (Optional[List[str]], optional): Column that represent a Document's page_content. Defaults to the first column.
+            metadata_columns (Optional[List[str]], optional): Column(s) that represent a Document's metadata.. Defaults to None.
+            metadata_json_column (Optional[str], optional): Column to store metadata as JSON. Defaults to "langchain_metadata".
+            format (Optional[str], optional): Format of page content (OneOf: text, csv, YAML, JSON). Defaults to 'text'.
+            formatter (Optional[Callable], optional): A function to format page content (OneOf: format, formatter). Defaults to None.
+
+        Returns:
+            AlloyDBLoader
+        """
         if table_name and query:
             raise ValueError("Only one of 'table_name' or 'query' should be specified.")
         if not table_name and not query:
             raise ValueError(
                 "At least one of the parameters 'table_name' or 'query' needs to be provided"
             )
-        self.content_columns = content_columns
-        self.metadata_columns = metadata_columns
         if format and formatter:
             raise ValueError("Only one of 'format' or 'formatter' should be specified.")
 
         if format and format not in ["csv", "text", "JSON", "YAML"]:
             raise ValueError("format must be type: 'csv', 'text', 'JSON', 'YAML'")
-        self.format = format or DEFAULT_FORMAT
-        self.formatter = formatter
-        self.read_only = read_only
-        self.time_out = time_out
-        self.metadata_json_column = metadata_json_column
+        if formatter:
+            formatter = formatter
+        elif format == "csv":
+            formatter = cvs_formatter
+        elif format == "YAML":
+            formatter = yaml_formatter
+        elif format == "JSON":
+            formatter = json_formatter
+        else:
+            formatter = text_formatter
+
+        if not query:
+            query = f'SELECT * FROM "{table_name}"'
+        stmt = sqlalchemy.text(query)
+
+        async with engine._engine.connect() as connection:
+            result_proxy = await connection.execute(stmt)
+
+            column_names = list(result_proxy.keys())
+            # Select content or default to first column
+            content_columns = content_columns or [column_names[0]]
+            # Select metadata columns
+            metadata_columns = metadata_columns or [
+                col for col in column_names if col not in content_columns
+            ]
+            # Check validity of metadata json column
+            if metadata_json_column and metadata_json_column not in column_names:
+                raise ValueError(
+                    f"Column {metadata_json_column} not found in query result {column_names}."
+                )
+            # use default metadata json column if not specified
+            if metadata_json_column and metadata_json_column in column_names:
+                metadata_json_column = metadata_json_column
+            elif DEFAULT_METADATA_COL in column_names:
+                metadata_json_column = DEFAULT_METADATA_COL
+            else:
+                metadata_json_column = None
+
+            # check validity of other column
+            all_names = content_columns + metadata_columns
+            for name in all_names:
+                if name not in column_names:
+                    raise ValueError(
+                        f"Column {name} not found in query result {column_names}."
+                    )
+        return cls(
+            cls.__create_key,
+            engine,
+            query,
+            content_columns,
+            metadata_columns,
+            formatter,
+            metadata_json_column,
+        )
+
+    @classmethod
+    def create_sync(
+        cls,
+        engine: AlloyDBEngine,
+        query: Optional[str] = None,
+        table_name: Optional[str] = None,
+        content_columns: Optional[List[str]] = None,
+        metadata_columns: Optional[List[str]] = None,
+        metadata_json_column: Optional[str] = None,
+        format: Optional[str] = None,
+        formatter: Optional[Callable] = None,
+    ):
+        coro = cls.create(
+            engine,
+            query,
+            table_name,
+            content_columns,
+            metadata_columns,
+            metadata_json_column,
+            format,
+            formatter,
+        )
+        return engine.run_as_sync(coro)
 
     async def _collect_async_items(self, docs_generator):
         return [doc async for doc in docs_generator]
@@ -158,62 +259,18 @@ class AlloyDBLoader(BaseLoader):
         return documents
 
     async def aload(self) -> List[Document]:
+        """Load Alloydb data into Document objects."""
         return [doc async for doc in self.alazy_load()]
 
     def lazy_load(self) -> Iterator[Document]:
+        """Load AlloyDB data into Document objects lazily."""
         yield from self.engine.run_as_sync(self._collect_async_items(self.alazy_load()))
 
     async def alazy_load(self) -> AsyncIterator[Document]:
-        """Load Alloydb data into Document objects lazily."""
-        content_columns = self.content_columns
-        metadata_columns = self.metadata_columns
-
-        if self.query:
-            stmt = sqlalchemy.text(self.query)
-        else:
-            stmt = sqlalchemy.text(f'SELECT * FROM "{self.table_name}"')
-
+        """Load AlloyDB data into Document objects lazily."""
+        stmt = sqlalchemy.text(self.query)
         async with self.engine._engine.connect() as connection:
             result_proxy = await connection.execute(stmt)
-            # Get field type information.
-            # cursor.description is a sequence of 7-item sequences.
-            # Each of these sequences contains information describing one result column:
-            # - name, type_code, display_size, internal_size, precision, scale, null_ok
-            # The first two items (name and type_code) are mandatory, the other five are optional
-            # and are set to None if no meaningful values can be provided.
-            # link: https://peps.python.org/pep-0249/#description
-            # column_types = [
-            #     cast(tuple, field)[0:2]
-            #     for field in result_proxy.cursor.description
-            # ]
-            column_names = list(result_proxy.keys())
-            content_columns = self.content_columns or [column_names[0]]
-            metadata_columns = self.metadata_columns or [
-                col for col in column_names if col not in content_columns
-            ]
-
-            # check validity of metadata json column
-            if (
-                self.metadata_json_column
-                and self.metadata_json_column not in column_names
-            ):
-                raise ValueError(
-                    f"Column {self.metadata_json_column} not found in query result {column_names}."
-                )
-            # use default metadata json column if not specified
-            if self.metadata_json_column and self.metadata_json_column in column_names:
-                metadata_json_column = self.metadata_json_column
-            elif DEFAULT_METADATA_COL in column_names:
-                metadata_json_column = DEFAULT_METADATA_COL
-            else:
-                metadata_json_column = None
-            # check validity of other column
-            all_names = content_columns + metadata_columns
-            for name in all_names:
-                if name not in column_names:
-                    raise ValueError(
-                        f"Column {name} not found in query result {column_names}."
-                    )
             # load document one by one
             while True:
                 row = result_proxy.fetchone()
@@ -221,26 +278,20 @@ class AlloyDBLoader(BaseLoader):
                     break
 
                 row_data = {}
+                column_names = self.content_columns + self.metadata_columns
+                column_names += (
+                    [self.metadata_json_column] if self.metadata_json_column else []
+                )
                 for column in column_names:
                     value = getattr(row, column)
                     row_data[column] = value
-                if self.formatter:
-                    formatter = self.formatter
-                elif self.format == "csv":
-                    formatter = cvs_formatter
-                elif self.format == "YAML":
-                    formatter = yaml_formatter
-                elif self.format == "JSON":
-                    formatter = json_formatter
-                else:
-                    formatter = text_formatter
 
                 yield _parse_doc_from_row(
-                    content_columns,
-                    metadata_columns,
+                    self.content_columns,
+                    self.metadata_columns,
                     row_data,
-                    metadata_json_column,
-                    formatter,
+                    self.metadata_json_column,
+                    self.formatter,
                 )
 
 
