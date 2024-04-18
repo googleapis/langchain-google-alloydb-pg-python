0ea4039eefa79cd9568ec2bf6d23018d200e7fd1
Averi Kitsch <akitsch@google.com>
2024-04-18 14:32:23 -0700

    chore: use Owlbot

---
+++ owlbot.py
@@ -0,0 +1,80 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""This script is used to synthesize generated parts of this library."""
+
+import re
+
+import synthtool as s
+from synthtool import gcp
+
+common = gcp.CommonTemplates()
+# ----------------------------------------------------------------------------
+# Add templated files
+# ----------------------------------------------------------------------------
+templated_files = common.py_library()
+
+# Copy templates for releases
+s.move(
+    templated_files / ".github",
+    excludes=[
+        "CODEOWNERS",
+        "auto-label.yaml",
+        "snippet-bot.yml",
+        "workflows/lint.yml",
+        "workflows/unittest.yml",
+        "auto-approve.yml",
+        "header-checker-lint.yml",
+    ],
+)
+# s.move(templated_files / "README.rst")
+s.move(
+    templated_files / ".kokoro",
+    excludes=[
+        "continuous/**",
+        "presubmit/**",
+        "samples/**",
+        "publish-docs.sh",
+        "test-samples*.sh",
+        "requirements.in",
+        "requirements.txt",
+    ],
+)
+s.move(templated_files / ".trampolinerc")
+s.move(templated_files / ".gitignore")
+s.move(
+    templated_files / "docs",
+    excludes=[
+        "index.rst",
+        "multiprocessing.rst",
+        "summary_overview.md",
+    ],
+)
+s.replace(
+    ".kokoro/release.sh",
+    "python3 setup.py sdist bdist_wheel",
+    "python3 -m build --wheel",
+)
+s.replace(
+    ".kokoro/docs/common.cfg", '"docs-staging-v2-staging', '"docs-staging-v2'
+)
+# s.replace(".gitignore", "bigquery/docs/generated", "package-lock.json")
+s.replace(
+    ".kokoro/docker/docs/Dockerfile",
+    "RUN python3 -m pip",
+    """RUN python3 -m pip
+    # Install build requirements
+    COPY requirements.txt /requirements.txt
+    RUN python3 -m pip install --require-hashes -r requirements.txt""",
+)
