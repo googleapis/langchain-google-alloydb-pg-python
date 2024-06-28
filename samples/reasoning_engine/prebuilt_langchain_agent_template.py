cbed497cbbebf26132c6d1132d5e25fd9a7a8c00
Averi Kitsch <akitsch@google.com>
2024-06-28 10:54:26 -0700

    add headers

--- samples/reasoning_engine/prebuilt_langchain_agent_template.py
+++ samples/reasoning_engine/prebuilt_langchain_agent_template.py
@@ -1,3 +1,16 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import os
 
 import vertexai
@@ -67,9 +80,7 @@ def similarity_search(query: str):
 # print(app.query(input="movies about engineers"))
 
 # Initialize VertexAI
-vertexai.init(
-    project=PROJECT_ID, location="us-central1", staging_bucket=STAGING_BUCKET
-)
+vertexai.init(project=PROJECT_ID, location="us-central1", staging_bucket=STAGING_BUCKET)
 
 # Deploy to VertexAI
 DISPLAY_NAME = os.getenv("DISPLAY_NAME") or "PrebuiltAgent"
