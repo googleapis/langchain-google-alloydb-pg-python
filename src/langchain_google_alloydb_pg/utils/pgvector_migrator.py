d6690cf22aaca6cd8804f069cbb2661ed13123e6
Twisha Bansal <twishabansal@google.com>
2024-09-19 16:25:17 +0530

    Lint fix

--- src/langchain_google_alloydb_pg/utils/pgvector_migrator.py
+++ src/langchain_google_alloydb_pg/utils/pgvector_migrator.py
@@ -12,8 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import json
-from typing import List, Optional, Sequence, TypeVar
 import warnings
+from typing import List, Optional, Sequence, TypeVar
+
 from sqlalchemy import RowMapping, text
 from sqlalchemy.exc import ProgrammingError
 
