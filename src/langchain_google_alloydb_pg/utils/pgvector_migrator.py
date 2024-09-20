7c279360ada7c6d54e8a34c2e4c60ee59bd43467
Twisha Bansal <twishabansal@google.com>
2024-09-20 12:37:17 +0530

    Fixed linting errors

--- src/langchain_google_alloydb_pg/utils/pgvector_migrator.py
+++ src/langchain_google_alloydb_pg/utils/pgvector_migrator.py
@@ -11,8 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import json
 import asyncio
+import json
 import warnings
 from typing import AsyncIterator, Iterator, List, Optional, Sequence, TypeVar
 
