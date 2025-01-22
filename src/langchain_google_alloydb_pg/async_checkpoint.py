99f27e08f260b997485a96c2e795eb1f2e404738
Fernando Omar Salazar Ortiz <fernandoomar@google.com>
2025-01-21 19:51:16 -0600

    feat(langgraph-base-alist): add async list method + tests

--- src/langchain_google_alloydb_pg/async_checkpoint.py
+++ src/langchain_google_alloydb_pg/async_checkpoint.py
@@ -13,9 +13,9 @@
 # limitations under the License.
 
 from contextlib import asynccontextmanager
+import json
 
-
-from typing import Any, Optional, cast
+from typing import Any, Optional, cast, Sequence, Tuple, AsyncIterator
 
 from sqlalchemy import text
 from sqlalchemy.ext.asyncio import AsyncEngine
@@ -23,20 +23,58 @@ from sqlalchemy.ext.asyncio import AsyncEngine
 from langchain_core.runnables import RunnableConfig
 
 from langgraph.checkpoint.base import (
+    WRITES_IDX_MAP,
     BaseCheckpointSaver,
     ChannelVersions,
     Checkpoint,
-    CheckpointMetadata
+    CheckpointMetadata,
+    CheckpointTuple,
+    get_checkpoint_id,
 )
 from langgraph.checkpoint.serde.jsonplus import JsonPlusSerializer
 
 from langgraph.checkpoint.serde.base import SerializerProtocol
+from langgraph.checkpoint.serde.types import TASKS
 
 from .engine import (
     CHECKPOINTS_TABLE,
     CHECKPOINT_WRITES_TABLE,
     AlloyDBEngine
 )
+
+MetadataInput = Optional[dict[str, Any]]
+
+# Select SQL used in `alist` method
+SELECT = f"""
+select
+    thread_id,
+    checkpoint,
+    checkpoint_ns,
+    checkpoint_id,
+    parent_checkpoint_id,
+    metadata,
+    (
+        select array_agg(array[bl.channel::bytea, bl.type::bytea, bl.blob])
+        from jsonb_each_text(checkpoint -> 'channel_versions')
+    ) as channel_values,
+    (
+        select
+        array_agg(array[cw.task_id::text::bytea, cw.channel::bytea, cw.type::bytea, cw.blob] order by cw.task_id, cw.idx)
+        from checkpoint_writes cw
+        where cw.thread_id = checkpoints.thread_id
+            and cw.checkpoint_ns = checkpoints.checkpoint_ns
+            and cw.checkpoint_id = checkpoints.checkpoint_id
+    ) as pending_writes,
+    (
+        select array_agg(array[cw.type::bytea, cw.blob] order by cw.task_path, cw.task_id, cw.idx)
+        from checkpoint_writes cw
+        where cw.thread_id = checkpoints.thread_id
+            and cw.checkpoint_ns = checkpoints.checkpoint_ns
+            and cw.checkpoint_id = checkpoints.parent_checkpoint_id
+            and cw.channel = '{TASKS}'
+    ) as pending_sends
+from checkpoints
+"""
 
 
 class AsyncAlloyDBSaver(BaseCheckpointSaver[str]):
@@ -151,6 +189,122 @@ class AsyncAlloyDBSaver(BaseCheckpointSaver[str]):
         # NOTE: we're using JSON serializer (not msgpack), so we need to remove null characters before writing
         return serialized_metadata.decode().replace("\\u0000", "")
     
+    def _dump_writes(
+        self,
+        thread_id: str,
+        checkpoint_ns: str,
+        checkpoint_id: str,
+        task_id: str,
+        task_path: str,
+        writes: Sequence[tuple[str, Any]],
+    ) -> list[dict[str, Any]]:
+        return [
+            {
+                "thread_id": thread_id,
+                "checkpoint_ns": checkpoint_ns,
+                "checkpoint_id": checkpoint_id,
+                "task_id": task_id,
+                "task_path": task_path,
+                "idx": WRITES_IDX_MAP.get(channel, idx),
+                "channel": channel,
+                "type": self.serde.dumps_typed(value)[0],
+                "blob": self.serde.dumps_typed(value)[1]
+            }
+                for idx, (channel, value) in enumerate(writes)
+        ]
+    
+    def _load_blobs(
+        self, blob_values: list[tuple[bytes, bytes, bytes]]
+    ) -> dict[str, Any]:
+        if not blob_values:
+            return {}
+        return {
+            k.decode(): self.serde.loads_typed((t.decode(), v))
+            for k, t, v in blob_values
+            if t.decode() != "empty"
+        }
+
+    def _load_checkpoint(
+        self,
+        checkpoint: dict[str, Any],
+        channel_values: list[tuple[bytes, bytes, bytes]],
+        pending_sends: list[tuple[bytes, bytes]],
+    ) -> Checkpoint:
+        return Checkpoint(
+            v = checkpoint["v"],
+            ts = checkpoint["ts"],
+            id = checkpoint["id"],
+            channel_values = self._load_blobs(channel_values),
+            channel_versions = checkpoint["channel_versions"].copy(),
+            versions_seen = {k: v.copy() for k, v in checkpoint["versions_seen"].items()},
+            pending_sends = [
+                self.serde.loads_typed((c.decode(), b)) for c, b in pending_sends or []
+            ]
+        )
+    
+    def _load_metadata(self, metadata: str) -> CheckpointMetadata:
+        return self.jsonplus_serde.loads(self.jsonplus_serde.dumps(metadata))
+    
+    def _load_writes(
+        self, writes: list[tuple[bytes, bytes, bytes, bytes]]
+    ) -> list[tuple[str, str, Any]]:
+        return (
+            [
+                (
+                    tid.decode(),
+                    channel.decode(),
+                    self.serde.loads_typed((t.decode(), v)),
+                )
+                for tid, channel, t, v in writes
+            ]
+            if writes
+            else []
+        )
+    
+    def _search_where(
+        self,
+        config: Optional[RunnableConfig],
+        filter: MetadataInput,
+        before: Optional[RunnableConfig] = None,
+    ) -> tuple[str, list[Any]]:
+        """Return WHERE clause predicates for alist() given config, filter, before.
+
+        This method returns a tuple of a string and a tuple of values. The string
+        is the parametered WHERE clause predicate (including the WHERE keyword):
+        "WHERE column1 = $1 AND column2 IS $2". The list of values contains the
+        values for each of the corresponding parameters.
+        """
+        wheres = []
+        param_values = []
+
+        # construct predicate for config filter
+        if config:
+            wheres.append("thread_id = %s ")
+            param_values.append(config["configurable"]["thread_id"])
+            checkpoint_ns = config["configurable"].get("checkpoint_ns")
+            if checkpoint_ns is not None:
+                wheres.append("checkpoint_ns = %s")
+                param_values.append(checkpoint_ns)
+
+            if checkpoint_id := get_checkpoint_id(config):
+                wheres.append("checkpoint_id = %s ")
+                param_values.append(checkpoint_id)
+
+        # construct predicate for metadata filter
+        if filter:
+            wheres.append("metadata @> %s ")
+            param_values.append(json.dumps(filter))
+
+        # construct predicate for `before`
+        if before is not None:
+            wheres.append("checkpoint_id < %s ")
+            param_values.append(get_checkpoint_id(before))
+
+        return (
+            "WHERE " + " AND ".join(wheres) if wheres else "",
+            param_values,
+        )
+    
     async def aput(
         self,
         config: RunnableConfig,
@@ -220,3 +374,107 @@ class AsyncAlloyDBSaver(BaseCheckpointSaver[str]):
             await conn.commit()
 
         return next_config
+    
+    async def aput_writes(
+        self,
+        config: RunnableConfig,
+        writes: Sequence[Tuple[str, Any]],
+        task_id: str,
+        task_path: str = ""
+    ) -> None:
+        """Asynchronously store intermediate writes linked to a checkpoint.
+
+        Args:
+            config (RunnableConfig): Configuration of the related checkpoint.
+            writes (List[Tuple[str, Any]]): List of writes to store.
+            task_id (str): Identifier for the task creating the writes.
+            task_path (str): Path of the task creating the writes.
+        
+            Returns:
+                None
+        """
+        upsert = f"""INSERT INTO "{self.schema_name}".{CHECKPOINT_WRITES_TABLE}(thread_id, checkpoint_ns, checkpoint_id, task_id, idx, channel, type, blob)
+                    VALUES (:thread_id, :checkpoint_ns, :checkpoint_id, :task_id, :idx, :channel, :type, :blob)
+                    ON CONFLICT (thread_id, checkpoint_ns, checkpoint_id, task_id, idx) DO UPDATE SET
+                    channel = EXCLUDED.channel,
+                        type = EXCLUDED.type,
+                        blob = EXCLUDED.blob;
+                """
+        insert = f"""INSERT INTO "{self.schema_name}".{CHECKPOINT_WRITES_TABLE}(thread_id, checkpoint_ns, checkpoint_id, task_id, idx, channel, type, blob)
+                    VALUES (:thread_id, :checkpoint_ns, :checkpoint_id, :task_id, :idx, :channel, :type, :blob)
+                    ON CONFLICT (thread_id, checkpoint_ns, checkpoint_id, task_id, idx) DO NOTHING
+                """
+        query = upsert if all(w[0] in WRITES_IDX_MAP for w in writes) else insert
+
+        params = self._dump_writes(
+            config["configurable"]["thread_id"],
+            config["configurable"]["checkpoint_ns"],
+            config["configurable"]["checkpoint_id"],
+            task_id,
+            task_path,
+            writes,
+        )
+
+        async with self.pool.connect() as conn:
+            await conn.execute(
+                text(query),
+                params,
+            )
+            await conn.commit()
+    
+    async def alist(
+        self,
+        config: Optional[RunnableConfig],
+        *,
+        filter: Optional[dict[str, Any]] = None,
+        before: Optional[RunnableConfig] = None,
+        limit: Optional[int] = None,
+    ) -> AsyncIterator[CheckpointTuple]:
+        """Asynchronously list checkpoints that match the given criteria.
+
+        Args:
+            config (Optional[RunnableConfig]): Base configuration for filtering checkpoints.
+            filter (Optional[Dict[str, Any]]): Additional filtering criteria for metadata.
+            before (Optional[RunnableConfig]): List checkpoints created before this configuration.
+            limit (Optional[int]): Maximum number of checkpoints to return.
+
+        Returns:
+            AsyncIterator[CheckpointTuple]: Async iterator of matching checkpoint tuples.
+        """
+
+        where, args = self._search_where(config, filter, before)
+        query = SELECT + where + " ORDER BY checkpoint_id DESC"
+        if limit:
+            query += f" LIMIT {limit}"
+        
+        async with self.pool.connect() as conn:
+            result = await conn.stream(text(query), args)
+            async for row in result:
+                value = dict(row._mapping)
+                yield CheckpointTuple(
+                    config = {
+                        "configurable": {
+                            "thread_id": value["thread_id"],
+                            "checkpoint_ns": value["checkpoint_ns"],
+                            "checkpoint_id": value["checkpoint_id"],
+                        }
+                    },
+                    checkpoint = self._load_checkpoint(
+                        value["checkpoint"],
+                        value["channel_values"],
+                        value["pending_sends"],
+                    ),
+                    metadata = self._load_metadata(value["metadata"]),
+                    parent_config = (
+                        {
+                            "configurable": {
+                                "thread_id": value["thread_id"],
+                                "checkpoint_ns": value["checkpoint_ns"],
+                                "checkpoint_id": value["parent_checkpoint_id"],
+                            }
+                        }
+                        if value["parent_checkpoint_id"]
+                        else None
+                    ),
+                    pending_writes = self._load_writes(value["pending_writes"]),
+                )
--- tests/test_async_checkpoint.py
+++ tests/test_async_checkpoint.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 import os
-from typing import Sequence
+from typing import Sequence, Tuple, Any
 
 import pytest
 import pytest_asyncio
@@ -26,7 +26,9 @@ from langgraph.checkpoint.base import (
     BaseCheckpointSaver,
     ChannelVersions,
     Checkpoint,
-    CheckpointMetadata
+    CheckpointMetadata,
+    create_checkpoint,
+    empty_checkpoint,
 )
 
 from langchain_google_alloydb_pg.engine import (
@@ -38,16 +40,56 @@ from langchain_google_alloydb_pg.async_checkpoint import (
     AsyncAlloyDBSaver,
 )
 
-write_config:RunnableConfig = {"configurable": {"thread_id": "1", "checkpoint_ns": ""}}
-read_config:RunnableConfig = {"configurable": {"thread_id": "1"}}
-
 project_id = os.environ["PROJECT_ID"]
 region = os.environ["REGION"]
 cluster_id = os.environ["CLUSTER_ID"]
 instance_id = os.environ["INSTANCE_ID"]
 db_name = os.environ["DATABASE_ID"]
 
-checkpoint:Checkpoint = {
+async def aexecute(engine: AlloyDBEngine, query: str) -> None:
+    async with engine._pool.connect() as conn:
+        await conn.execute(text(query))
+        await conn.commit()
+
+async def afetch(engine: AlloyDBEngine, query: str) -> Sequence[RowMapping]:
+    async with engine._pool.connect() as conn:
+        result = await conn.execute(text(query))
+        result_map = result.mappings()
+        result_fetch = result_map.fetchall()
+    return result_fetch
+
+@pytest.fixture
+def test_data():
+    """Fixture providing test data for checkpoint tests."""
+    config_0: RunnableConfig = {
+        "configurable": {
+            "thread_id": "1", 
+            "checkpoint_ns": ""
+        }
+    }
+    config_1: RunnableConfig = {
+        "configurable": {
+            "thread_id": "thread-1",
+            # for backwards compatibility testing
+            "thread_ts": "1",
+            "checkpoint_ns": "",
+        }
+    }
+    config_2: RunnableConfig = {
+        "configurable": {
+            "thread_id": "thread-2",
+            "checkpoint_id": "2",
+            "checkpoint_ns": "",
+        }
+    }
+    config_3: RunnableConfig = {
+        "configurable": {
+            "thread_id": "thread-2",
+            "checkpoint_id": "2-inner",
+            "checkpoint_ns": "inner",
+        }
+    }
+    chkpnt_0: Checkpoint = {
     "v": 1,
     "ts": "2024-07-31T20:14:19.804150+00:00",
     "id": "1ef4f797-8335-6428-8001-8a1503f9b875",
@@ -65,18 +107,29 @@ checkpoint:Checkpoint = {
     },
     "pending_sends": [],
 }
+    chkpnt_1: Checkpoint = empty_checkpoint()
+    chkpnt_2: Checkpoint = create_checkpoint(chkpnt_1, {}, 1)
+    chkpnt_3: Checkpoint = empty_checkpoint()
 
-async def aexecute(engine: AlloyDBEngine, query: str) -> None:
-    async with engine._pool.connect() as conn:
-        await conn.execute(text(query))
-        await conn.commit()
+    metadata_1: CheckpointMetadata = {
+        "source": "input",
+        "step": 2,
+        "writes": {},
+        "parents": 1,
+    }
+    metadata_2: CheckpointMetadata = {
+        "source": "loop",
+        "step": 1,
+        "writes": {"foo": "bar"},
+        "parents": None,
+    }
+    metadata_3: CheckpointMetadata = {}
 
-async def afetch(engine: AlloyDBEngine, query: str) -> Sequence[RowMapping]:
-    async with engine._pool.connect() as conn:
-        result = await conn.execute(text(query))
-        result_map = result.mappings()
-        result_fetch = result_map.fetchall()
-    return result_fetch
+    return {
+        "configs": [config_0, config_1, config_2, config_3],
+        "checkpoints": [chkpnt_0, chkpnt_1, chkpnt_2, chkpnt_3],
+        "metadata": [metadata_1, metadata_2, metadata_3],
+    }
 
 @pytest_asyncio.fixture
 async def async_engine():
@@ -97,8 +150,14 @@ async def async_engine():
 @pytest.mark.asyncio
 async def test_checkpoint_async(
     async_engine: AlloyDBEngine,
+    test_data: dict[str, Any],
 ) -> None:
     checkpointer = await AsyncAlloyDBSaver.create(async_engine)
+
+    configs = test_data["configs"]
+    checkpoints = test_data["checkpoints"]
+    metadata = test_data["metadata"]
+
     test_config = {
         'configurable': {
             'thread_id': '1',
@@ -106,8 +165,9 @@ async def test_checkpoint_async(
             'checkpoint_id': '1ef4f797-8335-6428-8001-8a1503f9b875'
         }
     }
+
     # Verify if updated configuration after storing the checkpoint is correct
-    next_config = await checkpointer.aput(write_config, checkpoint, {}, {})
+    next_config = await checkpointer.aput(configs[0], checkpoints[0], {}, {})
     assert dict(next_config) == test_config
 
     # Verify if the checkpoint is stored correctly in the database
@@ -116,3 +176,64 @@ async def test_checkpoint_async(
     for row in results:
         assert isinstance(row["thread_id"], str)
     await aexecute(async_engine, f"TRUNCATE TABLE {CHECKPOINTS_TABLE}")
+
+    writes: Sequence[Tuple[str,Any]] = [("test_channel1", {}), ("test_channel2", {})]
+
+    await checkpointer.aput_writes(configs[0], writes, task_id="1")
+    # Verify if the checkpoint writes are stored correctly in the database
+    results = await afetch(async_engine, f"SELECT * FROM {CHECKPOINT_WRITES_TABLE}")
+    assert len(results) == 2
+    for row in results:
+        assert isinstance(row["task_id"], str)
+    await aexecute(async_engine, f"TRUNCATE TABLE {CHECKPOINT_WRITES_TABLE}")
+
+    await checkpointer.aput(configs[1], checkpoints[1], metadata[0], {})
+    await checkpointer.aput(configs[2], checkpoints[2], metadata[1], {})
+    await checkpointer.aput(configs[3], checkpoints[3], metadata[2], {})
+
+    # call method / assertions
+    query_1 = {"source": "input"}  # search by 1 key
+    query_2 = {"step": 1, "writes": {"foo": "bar"},}  # search by multiple keys
+    query_3: dict[str, Any] = {}  # search by no keys, return all checkpoints
+    query_4 = {"source": "update", "step": 1}  # no match
+
+    search_results_1 = [c async for c in checkpointer.alist(None, filter=query_1)]
+    assert len(search_results_1) == 1
+    assert search_results_1[0].metadata == metadata[0]
+
+    search_results_2 = [c async for c in checkpointer.alist(None, filter=query_2)]
+    assert len(search_results_2) == 1
+    assert search_results_2[0].metadata == metadata[1]
+
+    search_results_3 = [c async for c in checkpointer.alist(None, filter=query_3)]
+    assert len(search_results_3) == 3
+
+    search_results_4 = [c async for c in checkpointer.alist(None, filter=query_4)]
+    assert len(search_results_4) == 0
+
+    # search by config (defaults to checkpoints across all namespaces)
+    search_results_5 = [
+        c async for c in checkpointer.alist({"configurable": {"thread_id": "thread-2"}})
+    ]
+    assert len(search_results_5) == 2
+    assert {
+        search_results_5[0].config["configurable"]["checkpoint_ns"],
+        search_results_5[1].config["configurable"]["checkpoint_ns"],
+    } == {"", "inner"}
+
+@pytest.mark.asyncio
+async def test_null_chars(
+    async_engine: AlloyDBEngine,
+    test_data: dict[str, Any],
+) -> None:
+        checkpointer = await AsyncAlloyDBSaver.create(async_engine)
+        config = await checkpointer.aput(
+            test_data["configs"][0],
+            test_data["checkpoints"][0],
+            {"my_key": "\x00abc"}, # type: ignore
+            {},
+        )
+        # assert (await checkpointer.aget_tuple(config)).metadata["my_key"] == "abc"  # type: ignore
+        assert [c async for c in checkpointer.alist(None, filter={"my_key": "abc"})][
+            0
+        ].metadata["my_key"] == "abc" # type: ignore
